83523710ad222cd1282f5af43b98173439c26ac2
Dominik Stańczyk <stanczykdominik0@gmail.com>
2024-10-27 14:00:16 +0100

    Code cleaning

--- database/init.sql
+++ database/init.sql
@@ -1,25 +1,24 @@
--- Users table for Backend 1
+-- Users Backend 1
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(200) NOT NULL
 );
 
--- Favorite Quotes table for Backend 1
+-- Favourites for Backend 1
 CREATE TABLE IF NOT EXISTS favorite_quotes (
     id SERIAL PRIMARY KEY,
     quote_id INTEGER NOT NULL,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
--- Quotes table for Backend 2
+-- Quotes for Backend 2
 CREATE TABLE IF NOT EXISTS quotes (
     id SERIAL PRIMARY KEY,
     text VARCHAR(500) NOT NULL,
     author VARCHAR(100)
 );
 
--- Optionally, insert initial data
 INSERT INTO quotes (text, author) VALUES
 ('The only limit to our realization of tomorrow is our doubts of today.', 'Franklin D. Roosevelt'),
 ('In the end, we will remember not the words of our enemies, but the silence of our friends.', 'Martin Luther King Jr.'),
